81e5d95ff73a19820f4a169b9de30d0a1e2be986
borec <borec@mint>
2014-11-10 12:08:28 +0100

    UPDATE

--- inplace-mergesort.cpp
+++ inplace-mergesort.cpp
@@ -8,169 +8,165 @@
 
 using namespace std;
 
-int compare(int idx1, int idx2);
-void exchange(int idx1, int idx2);
-void assign(int idx, int val);
-int item(int idx);
+void exchange       (int idx1, int idx2);
+void mergesort      (int, int a[]);
+int lower           (int from, int to, int val);
+int upper           (int from, int to, int val);
+void insert_sort    (int from, int to);
+int gcd             (int m, int n);
+void rotate         (int from, int mid, int to);
+void rotate2         (int from, int mid, int to);
+void merge          (int from, int pivot, int to, int len1, int len2);
+void sort           (int from, int to);
+void sort           ();
+void PrintArr       (int* arr, int size);
 
-void mergesort(int, int a[]);
-int lower (int from, int to, int val);
-int upper (int from, int to, int val);
-void insert_sort (int from, int to);
-int gcd (int m, int n);
-void rotate (int from, int mid, int to);
-void merge(int from, int pivot, int to, int len1, int len2);
-void sort(int from, int to);
-void sort ();
-void PrintArr(int* arr, int size);
-
-int * MyArr;
+int * nums;
 int   numsNr  = 0;
 int   numsMax = INIT;
-   
-void PrintArr(int* arr, int size)
+
+int RECURSION_DEPTH = 12;
+int BETTER_ROTATION = 1;
+
+void Print()
 {
-  for (int i=0; i<size; i++)
-    cout << arr[i] << " ";
+    for (int i=0; i<numsNr; i++)
+        cout << nums[i] << " ";
+    cout << endl;
 }
 
-int main ( int argc, char *argv[] )
+int main ( int argc, char * argv [] )
 {
-    MyArr = ( int * ) malloc ( INIT * sizeof ( int ) );
+    switch ( argc )
+    {
+        case 3: 
+            RECURSION_DEPTH = ( strtoul ( argv [2], NULL, 10 ) ? strtoul ( argv [2], NULL, 10 ) : 12 );
+        case 2:
+		{
+			if ( argv [1] == "worse" )
+             BETTER_ROTATION = 0;
+		}
+    }
+    //printf ( "recdepth: %d better %d?\n", RECURSION_DEPTH, BETTER_ROTATION );
+    nums = ( int * ) malloc ( INIT * sizeof ( int ) );
 
-    while ( scanf ( "%d", MyArr + ( numsNr ++ ) ) != -1 )
+    while ( scanf ( "%d", nums + ( numsNr ++ ) ) != -1 )
     {
         if ( numsNr == numsMax )
-            MyArr = ( int * ) realloc ( MyArr, ( numsMax *= STEP ) * sizeof ( int ) );
+            nums = ( int * ) realloc ( nums, ( numsMax *= STEP ) * sizeof ( int ) );
         if ( getchar () == '\n' )
             break;
     }        
     
-  sort( 0, numsNr + 1 );
-  //PrintArr(MyArr, numsNr );
-  free ( MyArr );
+    sort( 0, numsNr );
+    //Print ();
+    free ( nums );
 }
 
-
-int compare(int idx1, int idx2)
+void exchange( int idx1, int idx2 )
 {
-  return MyArr[idx1] - MyArr[idx2];
-}
-
-void assign(int idx, int val)
-{
-  MyArr[idx] =val;
-}
-
-int item(int idx)
-{
-  return MyArr[idx];
-}
-
-
-void exchange(int idx1, int idx2)
-{
-  int tmp = MyArr[idx1];
-  MyArr[idx1] = MyArr[idx2];
-  MyArr[idx2] = tmp;
+  int tmp    = nums[idx1];
+  nums[idx1] = nums[idx2];
+  nums[idx2] = tmp;
 }
 
 int lower (int from, int to, int val) 
 { 
-  int len = to - from, half;
-  while (len > 0) 
-  {
-    half = len/2;
-    int mid= from + half;
-    if (compare (mid, val) < 0) 
+    int len = to - from, half;
+    while (len > 0) 
     {
-      from = mid+1;
-      len = len - half -1;
-    } 
-    else len = half;
-  }
-  return from;
+        half = len/2;
+        int mid= from + half;
+        if ( nums [mid]  < nums[val] ) 
+        {
+            from = mid+1;
+            len = len - half -1;
+        } 
+        else len = half;
+    }
+    return from;
 }
 
 int upper (int from, int to, int val) 
 {
-  int len = to - from, half;
-  while (len > 0) 
-  {
-    half = len/2;
-    int mid= from + half;
-    if (compare (val, mid) < 0)
-    len = half;
-    else 
+    int len = to - from, half;
+    while (len > 0) 
     {
-      from = mid+1;
-      len = len - half -1;
-    }
+        half = len/2;
+        int mid= from + half;
+        if ( nums [val]  < nums [mid] )
+            len = half;
+        else 
+        {
+            from = mid+1;
+            len = len - half -1;
+        }
   }
   return from;
 }
 
 void insert_sort (int from, int to) 
 {
-  if (to > from+1) 
-  {
-    for (int i = from+1; i < to; i++) 
+    if (to > from+1) 
     {
-      for (int j = i; j > from; j--) 
-      {
-        if (compare(j, j-1)<0)
-        exchange(j, j-1);
-        else break;
-      }
-    }
+        for (int i = from+1; i < to; i++) 
+        {
+            for (int j = i; j > from; j--) 
+            {
+                if (nums [j]  < nums[j-1])
+                    exchange(j, j-1);
+                else break;
+            }
+        }
   }
 }
 
-int gcd (int m, int n) 
+int gcd ( int m, int n ) 
 {
-  while (n!=0) { int t = m % n; m=n; n=t; }
+  while ( n!=0 ) { int t = m % n; m=n; n=t; }
   return m;
 }
 
-void reverse (int from, int to) 
+void reverse ( int from, int to ) 
 {
-  while (from < to) 
-  {
-    exchange(from++, to--);
-  }
+    while (from < to) 
+        exchange( from ++, to -- );
 }
 
-void rotate (int from, int mid, int to) 
+void rotate2 ( int from, int mid, int to) 
 {
-  /*  a less sophisticated but costlier version:
+  /*  a less sophisticated but costlier version: */
   reverse(from, mid-1);
   reverse(mid, to-1);
-  reverse(from, to-1);
-  */
-  if (from==mid || mid==to) return;
-  int n = gcd(to - from, mid - from);
-  while (n-- != 0) 
-  {
-    int val = item(from+n);
-    int shift = mid - from;
-    int p1 = from+n, p2=from+n+shift;
-    while (p2 != from + n) 
-    {
-      assign(p1, item(p2));
-      p1=p2;
-      if ( to - p2 > shift) p2 += shift;
-      else p2=from + (shift - (to - p2));
-    }
-    assign(p1, val);
-  }
+  reverse(from, to-1); 
 }
 
-void merge(int from, int pivot, int to, int len1, int len2) 
+void rotate (int from, int mid, int to)
+{
+    if (from==mid || mid==to) return;
+    int n = gcd(to - from, mid - from);
+    while (n-- != 0) 
+    {
+        int val = nums[from+n];
+        int shift = mid - from;
+        int p1 = from+n, p2=from+n+shift;
+        while (p2 != from + n) 
+        {
+            nums[p1] = nums[p2];
+            p1=p2;
+            if ( to - p2 > shift) p2 += shift;
+            else p2=from + (shift - (to - p2));
+        }
+        nums[p1] = val;
+    }
+}
+
+void merge (int from, int pivot, int to, int len1, int len2) 
 {
   if (len1 == 0 || len2==0) return;
   if (len1+len2 == 2) 
   {
-    if (compare(pivot, from) < 0)
+    if (nums [pivot]  < nums[from])
     exchange(pivot, from);
     return;
   }
@@ -190,7 +186,8 @@ void merge(int from, int pivot, int to, int len1, int len2)
     first_cut = upper(from, pivot, second_cut);
     len11=first_cut - from;
   }
-  rotate(first_cut, pivot, second_cut);
+  
+  BETTER_ROTATION ? rotate(first_cut, pivot, second_cut) : rotate(first_cut, pivot, second_cut) ;
   int new_mid=first_cut+len22;
   merge(from, first_cut, new_mid, len11, len22);
   merge(new_mid, second_cut, to, len1 - len11, len2 - len22);
@@ -198,14 +195,14 @@ void merge(int from, int pivot, int to, int len1, int len2)
  
 void sort(int from, int to) 
 {
-  if (to - from < 12) 
-  {
-   insert_sort (from, to);
-   return;
-  }
-  int middle = (from + to)/2;
-  sort (from, middle);
-  sort (middle, to);
-  merge(from, middle, to, middle-from, to - middle);
+    if (to - from < RECURSION_DEPTH ) 
+    {
+        insert_sort (from, to);
+        return;
+    }
+    int middle = (from + to)/2;
+    sort (from, middle);
+    sort (middle, to);
+    merge(from, middle, to, middle-from, to - middle);
  }
  
--- job.sh
+++ job.sh
@@ -1,11 +1,59 @@
 #!/bin/bash
 
-ALGOS=(radixsort mergesort inplace-mergesort);
+#  ===========================================================================
+# |                                                                           |
+# |             COMMAND FILE FOR SUBMITTING SGE JOBS                          |
+# |                                                                           |
+# |                                                                           |
+# | SGE keyword statements begin with #$                                      |
+# |                                                                           |
+# | Comments begin with #                                                     |
+# | Any line whose first non-blank character is a pound sign (#)              |
+# | and is not a SGE keyword statement is regarded as a comment.              |
+#  ===========================================================================
+
+# Request Bourne shell as shell for job
+#$ -S /bin/bash
+
+# Execute the job from the current working directory.
+#$ -cwd
+
+# Defines  or  redefines  the  path used for the standard error stream of the job.
+#$ -e .
+
+# The path used for the standard output stream of the job.
+#$ -o .
+
+# Do not change.
+#$ -pe ompi 1
+
+# Do not change.
+#$ -q 12c_1slots_per_host.q
+
+ALGOS=(radixsort10 radixsort8192 mergesort inplace-mergesort);
 TESTS=(ran dis rev asc rdi);
-FROM=$((2**16));
-TO=$((2**23));
+FROM=$((2**14));
+TO=$((2**20));
+
+REC_FROM=2
+REC_TO=100
+REC_STEP=12
+
+OPTIMALIZATIONS=(
+"-O2 -mtune=generic" 
+"-O3 -mtune=generic" 
+"-Ofast -mtune=generic" 
+"-O3 -msse4.2 -mfpmath=sse -mtune=generic" 
+"-O3 -msse4.2 -mfpmath=sse -mtune=generic" 
+"-O3 -msse4.2 -mfpmath=sse -mfancy-math-387 -mtune=native" 
+"-O3 -msse4.2 -mfpmath=sse -mfancy-math-387 -march=native" 
+"-O3 -msse4.2 -mfpmath=sse -mfancy-math-387 -march=native -mregparm=3 -msseregparm" 
+"-O3 -msse4.2 -mfpmath=sse -mfancy-math-387 -march=native -mregparm=3 -msseregparm -fno-finite-math-only -fno-trapping-math -ffast-math -mrecip=all"
+);
+
+
 echo "COMPILING ALGOS";
-mkdir tmp;
+mkdir -p tmp;
 g++ -std=c++11 -o ./tmp/generator ./generator.cpp;
 for i in ${ALGOS[*]}
 do
@@ -17,12 +65,12 @@ for ((i=FROM;i<=TO;i*=2))
 do 
 	./tmp/generator $i      	       	        	> ./tmp/ran${i}.test;
 	./tmp/generator $i 1000 	       	        	> ./tmp/dis${i}.test;
-	./tmp/generator $i      | ./tmp/radixsort -v -r > ./tmp/rev${i}.test;
-	./tmp/generator $i      | ./tmp/radixsort -v    > ./tmp/asc${i}.test;
-	./tmp/generator $i 1000 | ./tmp/radixsort -v -r > ./tmp/rdi${i}.test;
+	./tmp/generator $i      | ./tmp/radixsort8192 -v -r > ./tmp/rev${i}.test;
+	./tmp/generator $i      | ./tmp/radixsort8192 -v    > ./tmp/asc${i}.test;
+	./tmp/generator $i 1000 | ./tmp/radixsort8192 -v -r > ./tmp/rdi${i}.test;
 done
 
-echo "RUNNING TESTS";
+echo "COMPARING ALGOS";
 for i in ${ALGOS[*]}
 do
 	for ((j=FROM;j<=TO;j*=2)) 
@@ -39,14 +87,15 @@ done
 for i in ${TESTS[*]}
 do
     gnuplot <<- EOF
-	set terminal svg size 800,600 fixed enhanced
-	set output '${i}.svg'	
+	set terminal svg size 1400,600 fixed enhanced
+	set output 'algos_${i}.svg'	
 	set origin 0.0,0.0
 	set ylabel "time"
 	set xlabel "N"
+	set xrange [*:]
 	set format x "%.0s*10^%T"
 	set logscale x	
-	set logscale y
+##    set logscale y
 	set key box left top
 	set multiplot
 	`echo -n "plot "
@@ -58,6 +107,105 @@ do
 	unset multiplot	
 	EOF
 done
+
+## compare of rotations
+
+for ((j=FROM;j<=TO;j*=2)) 
+do 
+		TIME=`/usr/bin/time -f "%e" ./tmp/inplace-mergesort worse < ./tmp/ran$j.test 2>&1`;
+		echo -e "ALGO: inplace-mergesort (worse rotation) TEST: ran$j\t TIME: $TIME";
+		echo -e $j"\t"$TIME >> ./tmp/"inplace-mergesort-worse_"ran.out;
+done 
+
+gnuplot <<- EOF
+set terminal svg size 1400,600 fixed enhanced
+set output 'rotations.svg'	
+set origin 0.0,0.0
+set ylabel "time"
+set xlabel "N"
+set xrange [*:]
+set format x "%.0s*10^%T"
+set logscale x	
+##set logscale y
+set key box left top
+set multiplot
+plot './tmp/inplace-mergesort_ran.out' using 1:2 title 'optimized rotations' smooth unique with line axes x1y2, './tmp/inplace-mergesort-worse_ran.out' using 1:2 title 'simple rotations' smooth unique with line axes x1y2	
+show key
+unset multiplot	
+EOF
+
+DEFAULT='-std=c++11 ./inplace-mergesort.cpp -o ./tmp/inplace-mergesort_opt';
+
+##compare compile options
+
+for ((i=0; i<9; i++))
+do
+	g++ ${OPTIMALIZATIONS[$i]} ${DEFAULT}${i};
+done
+
+for ((j=FROM;j<=TO;j*=2)) 
+do 
+	for ((i=0; i<9; i++))
+	do
+		TIME=`/usr/bin/time -f "%e" ./tmp/inplace-mergesort_opt$i < ./tmp/ran$j.test 2>&1`;
+		echo -e "OPTI: $i TEST: ran$j\t TIME: $TIME";
+		echo -e $j"\t"$TIME >> ./tmp/"inplace-mergesort_opt"$i.out;
+	done
+done
+
+gnuplot <<- EOF
+set terminal svg size 1400,600 fixed enhanced
+set output 'optimalizations.svg'	
+set origin 0.0,0.0
+set ylabel "time"
+set xlabel "N"
+set xrange [*:]
+set format x "%.0s*10^%T"
+set logscale x	
+##set logscale y
+set key box left bottom
+set multiplot
+`echo -n "plot "
+for ((i=0; i<9; i++))
+do
+	echo -n "'./tmp/"inplace-mergesort_opt"$i.out' using 1:2 title '${OPTIMALIZATIONS[$i]}' smooth unique with line axes x1y2, "
+done`	
+show key
+unset multiplot	
+EOF
+
+
+for ((j=FROM;j<=TO;j*=2)) 
+do 
+	for ((i=REC_FROM; i<REC_TO; i+=REC_STEP))
+	do
+		TIME=`/usr/bin/time -f "%e" ./tmp/inplace-mergesort dummy $i < ./tmp/ran$j.test 2>&1`;
+		echo -e "REC: $i TEST: ran$j\t TIME: $TIME";
+		echo -e $j"\t"$TIME >> ./tmp/"inplace-mergesort_rec"$i.out;
+	done
+done
+
+gnuplot <<- EOF
+set terminal svg size 1400,600 fixed enhanced
+set output 'recursion.svg'	
+set origin 0.0,0.0
+set ylabel "time"
+set xlabel "N"
+set xrange [*:]
+set format x "%.0s*10^%T"
+set logscale x	
+##set logscale y
+set key box left bottom
+set multiplot
+`echo -n "plot "
+for ((i=REC_FROM; i<REC_TO; i+=REC_STEP))
+do
+	echo -n "'./tmp/"inplace-mergesort_rec"$i.out' using 1:2 title 'recursion depth $i' smooth unique with line axes x1y2, "
+done`	
+show key
+unset multiplot	
+EOF
+
 rm -rf ./tmp;
 
 
